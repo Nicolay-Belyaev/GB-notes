ea3aff710172db7907d788a1738ebe8da9cad9b7
NicolayBelyaev <nickb2147@gmail.com>
2023-02-12 22:28:55 +0300

    first draw of UI

--- main.py
+++ main.py
@@ -1,7 +1,47 @@
 import CRUD
 import UI
 
-current_command = ""
-while current_command != "exit":
 
+current_command = ""
+while True:
+    current_command = input()
+    match current_command:
+        case "exit":
+            break
+        case "":
+            print("""Доступные команды:\n"
+                        add - добавить заметку,\n 
+                       find - поиск по произвольную параметру,\n 
+                     change - изменить заметку,\n
+                        del - удалить заметку""")
+        case "add":
+            print("Введите заголовок: ", end="")
+            title = input()
+            print("Введите текст", end="")
+            content = input()
+            CRUD.create(title, content)
+        case "find":
+            print("""Для поиска доступны следующие ключи:\n
+                     1 - ID,\n
+                     2 - заголовок\n
+                     3 - текст\n
+                     4 - дата создание\n
+                     5 - время создания""")
+            key = 0
+            while key not in [1, 2, 3, 4, 5]:
+                print("Введите ключ (1-5) для поиска: ")
+                key = input()
+            translation_dict = {
+                1: "ID",
+                2: "title",
+                3: "content",
+                4: "creationdate",
+                5: "creationtime"
+            }
+            search_key = translation_dict.get(key)
+            print("Введите значение: ")
+            search_value = input()
+            CRUD.read(search_key, search_value)
+        case "change":
+            print()
 
